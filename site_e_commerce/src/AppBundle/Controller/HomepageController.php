34327ecaa4778fe362f69358a728e44c60fcad8d
toure <tourekadiy@gmail.com>
2017-10-12 17:26:13 +0200

    test branch

---
+++ site_e_commerce/src/AppBundle/Controller/HomepageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Homepage controller.
+ *
+ * @Route("homepage")
+ */
+
+class HomepageController extends Controller
+{
+     /**
+     * Lists all category entities.
+     *
+     * @Route("/home", name = "home_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $products = $em->getRepository('AppBundle:product')->findAll();
+    }
+}
